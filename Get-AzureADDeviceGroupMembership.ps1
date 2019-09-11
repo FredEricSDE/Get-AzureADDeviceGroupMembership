49e3fbc6515fe9d9de738dfecb4b4319bdca13a0
Fred Eric S <frederic.sellmeier@sva.de>
2019-09-11 22:31:46 +0200

    MVP 1.0 - 15.08.2019

---
+++ Get-AzureADDeviceGroupMembership.ps1
@@ -0,0 +1,37 @@
+﻿# Check if connected to AzureAD. If not - connect.
+
+if($AzureADConnection.Account -eq $null)
+{
+    $AzureADConnection = Connect-AzureAD
+}
+
+# Prompt for Device Name
+
+$DeviceName = Read-Host -Prompt 'DeviceName to Search for'
+
+# Get All Groups and Devices, then check what Groups a certain Device is Member of...
+
+Function Get-AzureADDeviceGroupMembership
+{
+    $AzureAdGroups = Get-AzureADGroup -All $true
+    $AzureADDevices = Get-AzureADDevice -All $true
+    $Result = @()
+
+    Foreach ($Group in $AzureADGroups)
+    {
+        $Members = Get-AzureADGroupMember -ObjectId $Group.ObjectID
+    
+        Foreach ($Device in $AzureADDevices)
+        {
+            if (($Members.DeviceID -Match $Device.DeviceID) -and ($Device.DisplayName -like "$DeviceName"))
+            {
+                $Result += $Group.DisplayName
+            }
+
+        }
+    }
+
+    $Result | fl
+}
+
+Get-AzureADDeviceGroupMembership
